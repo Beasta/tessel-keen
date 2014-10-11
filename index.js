f087cc0f5744c1fb6f5f5d545676189649b0cf73
jialiya <jialiya.huang0@gmail.com>
2014-10-10 19:15:11 -0700

    keen demo

---
+++ index.js
@@ -0,0 +1,34 @@
+var Keen = require('keen.io');
+var tessel = require('tessel');
+// attach the accelerometer to port A
+var accel = require('accel-mma84').use(tessel.port['A']);
+var count = 0;
+
+console.log("setting up keen...");
+// set up keen with your APIs here
+var keen = Keen.configure({
+    projectId: "###",
+    writeKey: "###",
+    readKey: "###"
+});
+
+var dataArray = [];
+accel.on('ready', function () {
+  console.log("Accelerometer is ready!");
+  // as we get data push it into an array
+  accel.on('data', function (xyz) {
+    dataArray.push(xyz);
+  });
+});
+
+// every second send up all the accelerometer data
+setTimeout(function sendData(){
+  keen.addEvent("accel", {data: dataArray}, function(err){
+    if (err) throw err;
+
+    console.log("Added event #"+count, "data: ", dataArray);
+    count++;
+    dataArray = [];
+    setTimeout(sendData, 1000);
+  });
+}, 1000);
